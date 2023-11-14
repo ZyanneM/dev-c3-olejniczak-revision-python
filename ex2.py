bb2a5fe6bd050968af78d54a23e3d3d48274b099
Marilyn Olejniczak <92530551+ZyanneM@users.noreply.github.com>
2023-11-14 15:29:50 +0100

    Add string length display

--- ex2.py
+++ ex2.py
@@ -6,6 +6,6 @@ def StringData():
     elif lastChar not in ['.', '?', '!']:
         print("Votre phrase est incorrecte : elle doit se terminer par un signe de ponctuation.")
     else:
-        print("Félicitations!\nVotre phrase est valide!\nVoici votre phrase en majuscule : ", string.upper(), "\nVoici votre phrase en minuscule : ", string.lower())
+        print("Félicitations!\nVotre phrase est valide!\nVoici votre phrase en majuscule : ", string.upper(), "\nVoici votre phrase en minuscule : ", string.lower(),"\nDécouvrez même en exclusivité le nombre de mots de votre phrase : ", len(string.split()), "!")
 
 StringData()
