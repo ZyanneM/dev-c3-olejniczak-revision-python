7fc4ea13afd10a46737966882595920c567bd131
Marilyn Olejniczak <92530551+ZyanneM@users.noreply.github.com>
2023-11-14 16:12:28 +0100

    Save before modify verification logic because if my number has two or more characters it don't work

--- ex3.py
+++ ex3.py
@@ -1,20 +1,33 @@
+def handleOperation(number1, operator, number2):
+    if operator == 'x':
+        return number1 * number2
+    elif operator == '+':
+        return number1 + number2
+    elif operator == '-':
+        return number1 - number2
+    elif operator == '/':
+        return number1 / number2
+
 def Calculator(str):
     if len(str) == 3:
         if str[0].isdigit() and str[2].isdigit() and str[1] in ['+', '-', 'x', '/']:
             firstOperationNumberA = int(str[0])
             firstOperationNumberB = int(str[2])
-            if str[1] == 'x':
-                firstOperation = firstOperationNumberA * firstOperationNumberB
-            elif str[1] == '+':
-                firstOperation = firstOperationNumberA + firstOperationNumberB
-            elif str[1] == '-':
-                firstOperation = firstOperationNumberA - firstOperationNumberB
-            elif str[1] == '/':
-                firstOperation = firstOperationNumberA / firstOperationNumberB
+            firstOperation = handleOperation(firstOperationNumberA, str[1], firstOperationNumberB)
             print("Le résultat de l'opération est : ", firstOperation)
         else:
             print("Le premier ou le troisième caractère n'est pas un chiffre ou le deuxième caractère n'est pas un opérateur.")
+    elif len(str) == 5:
+        if str[0].isdigit() and str[2].isdigit() and str[4].isdigit() and str[1] in ['+', '-', 'x', '/'] and str[3] in ['+', '-', 'x', '/']:
+            firstOperationNumberA = int(str[0])
+            firstOperationNumberB = int(str[2])
+            secondOperationNumber = int(str[4])
+            firstOperation = handleOperation(firstOperationNumberA, str[1], firstOperationNumberB)
+            secondOperation = handleOperation(firstOperation, str[3], secondOperationNumber)
+            print("Le résultat de l'opération est : ", secondOperation)
+        else:
+            print("Le premier, le troisième ou le cinquième caractère n'est pas un chiffre ou les opérateurs sont incorrects.")
     else:
         print("Votre opération n'est pas valide")
 
-Calculator("3x3")
+Calculator("34+2")
