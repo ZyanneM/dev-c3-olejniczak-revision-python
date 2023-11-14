7feeb01880ba88f66e1857dda7f449ed46714792
Marilyn Olejniczak <92530551+ZyanneM@users.noreply.github.com>
2023-11-14 16:48:45 +0100

    Add logic to find largest number

--- exos/ex4.py
+++ exos/ex4.py
@@ -2,6 +2,12 @@ import random
 
 def numberList():
     random_numbers = [random.randint(1, 100) for _ in range(10)]
-    print(random_numbers)
+    print("Liste de 10 nombres aléatoires : ", random_numbers)
+    largest_number = random_numbers[0]
+    for num in random_numbers:
+        if num > largest_number:
+            largest_number = num
+    print("Le plus grand nombre de la liste est : ", largest_number)
+
 
 numberList()
