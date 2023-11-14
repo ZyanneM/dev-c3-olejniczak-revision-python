fa2f03ed24e209b6a6cde0bc52a6ae674b6cad6e
Marilyn Olejniczak <92530551+ZyanneM@users.noreply.github.com>
2023-11-14 17:03:16 +0100

    Add locale import and use strftime method to get string date in french

--- exos/ex6.py
+++ exos/ex6.py
@@ -1,8 +1,11 @@
 from datetime import datetime
+import locale
 
 def getDateTime():
+    locale.setlocale(locale.LC_TIME, "fr_FR")
     now = datetime.now()
+    formatted_date = now.strftime("%A %d %B %Y %H:%M:%S")
 
-    print("La date et l'heure actuelles sont : ", now)
+    print("La date et l'heure actuelles sont : ", formatted_date)
 
 getDateTime()
