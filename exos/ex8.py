59c78e894d84bf563c464b8a28688f5c1e95a306
Marilyn Olejniczak <92530551+ZyanneM@users.noreply.github.com>
2023-11-14 17:38:32 +0100

    Add function to get best student and verify the first name

--- exos/ex8.py
+++ exos/ex8.py
@@ -1,20 +1,22 @@
 import random
 
 def studentDictionary():
+    student_names = [("Naruto", "Uzumaki"), ("Sasuke", "Uchiwa"), ("Sakura", "Haruno"), ("Hinata", "Hyuga"), ("Ino", "Yamanaka"), ("Shikamaru", "Nara"), ("Shino", "Aburame"), ("Rock", "Lee"), ("Choji", "Akimichi"), ("Neji", "Hyuga")]
+
     students = {}
 
-    for i in range(1, 11):
-        student_name = f"Élève {i}"
+    for i, (first_name, last_name) in enumerate(student_names, start=1):
         notes = [random.randint(0, 20) for _ in range(3)]
-        students[student_name] = notes
+        student_key = f"Élève {i}: {first_name} {last_name}"
+        students[student_key] = notes
 
-    print("Voici les informations sur vos élèves :", students)
+    print("Voici les informations sur vos étudiants :", students)
 
     best_student = findBestStudent(students)
-    print(f"L'élève avec la meilleure note est : {best_student} avec une note de {max(students[best_student])}")
+    print(f"L'étudiant avec la meilleure note est : {best_student} avec une note de {max(students[best_student])}")
 
 def findBestStudent(students):
-    best_student = max(students, key=lambda student: sum(students[student]))
+    best_student = min(students, key=lambda student: (student.split(":")[1].split()[1], student.split(":")[1].split()[0]))
     return best_student
 
 studentDictionary()
