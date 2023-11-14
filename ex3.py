5dbc13ed7c267c242734d38b91c15d624ead1e26
Marilyn Olejniczak <92530551+ZyanneM@users.noreply.github.com>
2023-11-14 15:56:12 +0100

    Add first operation result

--- ex3.py
+++ ex3.py
@@ -3,9 +3,18 @@ def Calculator(str):
         if str[0].isdigit() and str[2].isdigit() and str[1] in ['+', '-', 'x', '/']:
             firstOperationNumberA = int(str[0])
             firstOperationNumberB = int(str[2])
+            if str[1] == 'x':
+                firstOperation = firstOperationNumberA * firstOperationNumberB
+            elif str[1] == '+':
+                firstOperation = firstOperationNumberA + firstOperationNumberB
+            elif str[1] == '-':
+                firstOperation = firstOperationNumberA - firstOperationNumberB
+            elif str[1] == '/':
+                firstOperation = firstOperationNumberA / firstOperationNumberB
+            print("Le résultat de l'opération est : ", firstOperation)
         else:
             print("Le premier ou le troisième caractère n'est pas un chiffre ou le deuxième caractère n'est pas un opérateur.")
     else:
         print("Votre opération n'est pas valide")
 
-Calculator("3x3m")
+Calculator("3x3")
