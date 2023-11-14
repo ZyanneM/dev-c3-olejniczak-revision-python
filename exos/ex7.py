5d0e73f03c8633df0841002547c90c5b040b9827
Marilyn Olejniczak <92530551+ZyanneM@users.noreply.github.com>
2023-11-14 17:17:26 +0100

    Add error exceptions

--- exos/ex7.py
+++ exos/ex7.py
@@ -7,3 +7,10 @@ def count_words(input_file, output_file):
                 output.write(f"Le nombre de mots dans le fichier est : {word_count}")
                 
         print(f"Le résultat a été écrit dans le fichier {output_file}")
+
+    except FileNotFoundError:
+        print(f"Le fichier {input_file} n'a pas été trouvé.")
+    except Exception as e:
+        print(f"Une erreur s'est produite : {e}")
+
+count_words("code_passion.txt", "output.txt")
