932fa8d46740d18a325b798df2658dafa41e7c87
Marilyn Olejniczak <92530551+ZyanneM@users.noreply.github.com>
2023-11-14 15:13:37 +0100

    Add logic to verify the string input

--- ex2.py
+++ ex2.py
@@ -1,4 +1,11 @@
 def StringData():
     string = input("Saisissez une phrase : ")
+    lastchar = string[-1]
+    if len(string) < 10:
+        print("La phrase est trop courte")
+    elif lastchar not in ['.', '?', '!']:
+        print("La phrase ne se termine pas par un signe de ponctuation")
+    elif not isinstance(string, str):
+        print("La phrase n'est pas une chaine de caractères")
 
 StringData()
